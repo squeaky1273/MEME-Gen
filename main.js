13dd87913eaed2667fd0d9bbffb0b17f14472985
squeaky1273 <pjriddell22@gmail.com>
2021-01-07 16:37:49 -0800

    temp change

--- main.js
+++ main.js
@@ -47,23 +47,25 @@ bFontSize.addEventListener('change', function() {
     bottomDisplay.style.fontSize = bFontSize.value + "px"
 })
 
-$("input").change(function(e) {
-
-    for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
-
-        var file = e.originalEvent.srcElement.files[i];
-
-        var img = document.createElement("img");
-        var reader = new FileReader();
-        reader.onloadend = function() {
-             img.src = reader.result;
-        }
-        reader.readAsDataURL(file);
-        $("input").after(img);
-    }
-});
-
 // Bottom Text Type Handler
 bFontType.addEventListener('change', function() {
     bottomDisplay.style.fontFamily = bFontType.value
 })
+
+
+// // from http://jsfiddle.net/ugPDx/
+// $("input").change(function(e) {
+
+//     for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+
+//         var file = e.originalEvent.srcElement.files[i];
+
+//         var img = document.createElement("img");
+//         var reader = new FileReader();
+//         reader.onloadend = function() {
+//              img.src = reader.result;
+//         }
+//         reader.readAsDataURL(file);
+//         $("input").after(img);
+//     }
+// });
