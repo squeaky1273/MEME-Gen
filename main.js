d2c6f89207d4c8f4456d716aa738a02dc45333ba
squeaky1273 <pjriddell22@gmail.com>
2021-01-08 21:43:16 -0800

    text over image

--- main.js
+++ main.js
@@ -1,52 +1,32 @@
-const tText = document.getElementById('tText')
-const tColor = document.getElementById('tColor')
-const tFontSize = document.getElementById('tFontSize')
-
-const bText = document.getElementById('bText')
-const bColor = document.getElementById('bColor')
-const bFontSize = document.getElementById('bFontSize')
-
 const fontType = document.getElementById('fontType')
-
-const topDisplay = document.getElementById('topDisplay')
-const bottomDisplay = document.getElementById('bottomDisplay')
-
-
-// Top Text Event Handler
-tText.addEventListener('change', function() {
-    topDisplay.innerHTML = tText.value
-})
-
-// Top Text Color Event Handler
-tColor.addEventListener('change', function() {
-    topDisplay.style.color = tColor.value
-})
-
-// Top Text Size Event Handler
-tFontSize.addEventListener('change', function() {
-    topDisplay.style.fontSize = tFontSize.value + "px"
-})
-
-// Bottom Text Event Handler
-bText.addEventListener('change', function() {
-    bottomDisplay.innerHTML = bText.value
-})
-
-// Bottom Text Color Handler
-bColor.addEventListener('change', function() {
-    bottomDisplay.style.color = bColor.value
-})
-
-// Bottom Text Size Handler
-bFontSize.addEventListener('change', function() {
-    bottomDisplay.style.fontSize = bFontSize.value + "px"
-})
 
 // Top & Bottom Text Type Handler
 fontType.addEventListener('change', function() {
     topDisplay.style.fontFamily = fontType.value
     bottomDisplay.style.fontFamily = fontType.value
 })
+
+function update_top_text(){
+    var top_text = document.getElementById("top_text");
+    var topDisplay = document.getElementById('topDisplay');
+    var tColor = document.getElementById('tColor');
+    var tFontSize = document.getElementById('tFontSize');
+
+    topDisplay.innerHTML = top_text.value;
+    topDisplay.style.color = tColor.value
+    topDisplay.style.fontSize = tFontSize.value + "px"
+}
+
+function update_bottom_text(){
+    var bottom_text = document.getElementById("bottom_text");
+    var bottomDisplay = document.getElementById('bottomDisplay');
+    var bColor = document.getElementById('bColor');
+    var bFontSize = document.getElementById('bFontSize');
+
+    bottomDisplay.innerHTML = bottom_text.value
+    bottomDisplay.style.color = bColor.value
+    bottomDisplay.style.fontSize = bFontSize.value + "px"
+}
 
 // from https://coderdojoscotland.com/wp-content/uploads/2018/03/CoderDojo-Meme-Website.pdf
 function update_image(){
